583c921963458d988869e57ce6468e46deda84a5
Clement Escoffier <clement.escoffier@gmail.com>
2024-07-12 09:58:10 +0200

    Transform the UI into a gateway redirecting to the backend services

--- fight-ui/src/main/webapp/src/config.js
+++ fight-ui/src/main/webapp/src/config.js
@@ -1,21 +1,8 @@
-let endpoints = {
-    'heroes': 'http://localhost:8080/api/heroes/random',
-    'villains': 'http://localhost:8081/api/villains/random',
-    'fight': 'http://localhost:8082/api/fights'
-};
-
-try {
-    const response = await fetch("/config/endpoints");
-    if (response.ok) {
-        endpoints = await response.json();
-    } else {
-        throw new Error(`The endpoint failed with status code ${response.status}`)
-    }
-} catch (err) { 
-    console.error(`Configuration endpoint failure, assuming local development: ${err}`)
-}
-
 export default {
-    endpoints
+    endpoints: {
+        'heroes': '/gateway/hero',
+        'villains': '/gateway/villain',
+        'fight': '/gateway/fight'
+    }
 }
 
